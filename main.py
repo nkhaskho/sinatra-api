16b3c25087cdfef848d19d97499d4e57abbe4ecb
Naiim <khaskhoussi.naiim@gmail.com>
2023-03-25 22:28:14 +0100

    Adding cors origins middleware
    Signed-off-by: Naiim <khaskhoussi.naiim@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-import urllib.request
 from fastapi.openapi.utils import get_openapi
 import routers.analytics
 import routers.preprocess
@@ -7,9 +6,25 @@ import routers.annotations
 import routers.repair
 import routers.merging
 import settings
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 def custom_openapi():
