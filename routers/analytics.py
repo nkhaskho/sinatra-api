79d9c92c15b6c3f85f3c4cf22b1c07d7d8a14380
Naiim Khaskhoussi <nkhaskho@gmail.com>
2023-05-14 20:38:48 +0100

    Fix analytics endpoint
    Signed-off-by: Naiim Khaskhoussi <nkhaskho@gmail.com>

--- routers/analytics.py
+++ routers/analytics.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from string import punctuation
 from fastapi import APIRouter, UploadFile, Body
 from .models.dataset import *
@@ -7,11 +8,24 @@ from .models.dataset import *
 SPECIAL_CHARS = ['@', '(', ')', '!']
 BIN_CHARS = ['y', 'n', 't', 'f', 'y', 'y.', 'n.', 't.', 'f.']
 # special null values
-SPECIAL_NULLS = ['nil', '?', 'null', 'Nan', 'nan']
+SPECIAL_NULLS = ['null', 'Nan', 'nan', 'nil', '?']
 
 spechars = "|".join(SPECIAL_CHARS)
 specialna = "|"
 
+async def df_analytics(df: pd.DataFrame):
+    analytics = DatasetAnalytics(
+        columns = df.shape[1], 
+        rows = df.shape[0],
+        nacount = sum(list(df.isna().sum())),
+        spechars = df[df.isin(SPECIAL_CHARS)].count(axis=0).sum(),
+        specialnas = df[df.isin(SPECIAL_NULLS)].count(axis=0).sum(),
+        binaries = df[df.isin(BIN_CHARS)].count(axis=0).sum(),
+        numerics = df.select_dtypes(include=np.number).count().sum(),
+        duplicates = list(df.duplicated()).count(True),
+        headers = list(df.columns),
+    )
+    return analytics
 
 router = APIRouter(prefix="/analytics", tags=["analytics"])
 
@@ -19,29 +33,8 @@ router = APIRouter(prefix="/analytics", tags=["analytics"])
 @router.get("/remote", response_model=DatasetAnalytics)
 async def get_analytics_from_remote(url: str, sep=";"):
     df = pd.read_csv(url, sep=sep)
-    nalist = list(df.isna().sum())
-    spechars_counts, bins, nums = 0, 0, 0
-    bins = df.isin(BIN_CHARS).sum(axis=1).count()
-    special_nas = df.isin(SPECIAL_NULLS).sum(axis=1).count()
-    for col in df.columns:
-        try:
-            nums += len(df[df[col] % 1 > 0])
-            spechars_counts += len(df[df[col].str.contains(spechars)])
-        except:
-            pass
-    analytics = DatasetAnalytics(
-        columns = df.shape[1], 
-        rows = df.shape[0],
-        nacount = sum(nalist), # na_counts 
-        specialnas = special_nas,
-        numerics = nums,
-        headers = list(df.columns),
-        duplicates = list(df.duplicated()).count(True),
-        spechars = spechars_counts,
-        binaries = bins
-    )
+    analytics = await df_analytics(df)
     return analytics
-
 
 
 @router.post("/local")
@@ -49,26 +42,5 @@ async def get_analytics_from_upload(file: UploadFile, sep=","):
     if not file.filename.endswith('.csv'):
         return {"error": "invalid file extension"}
     df = pd.read_csv(file.file, sep=sep)
-    nalist = list(df.isna().sum())
-    spechars_counts, bins, nums = 0, 0, 0
-    special_nas = df.isin(SPECIAL_NULLS).sum(axis=1).count()
-    bins = df.isin(BIN_CHARS).sum(axis=1).count()
-    for col in df.columns:
-        try:
-            nums += len(df[(df[col] % 1) > 0])
-            nums += len(df[df[col].str.contains('.')]) 
-            spechars_counts += len(df[df[col].str.contains(spechars)])
-        except:
-            print('Exception')
-    analytics = DatasetAnalytics(
-        columns = df.shape[1], 
-        rows = df.shape[0],
-        nacount = sum(nalist),
-        specialnas = special_nas,
-        numerics = nums,
-        duplicates = list(df.duplicated()).count(True),
-        spechars = spechars_counts,
-        binaries = bins,
-        headers = list(df.columns),
-    )
+    analytics = await df_analytics(df)
     return analytics
