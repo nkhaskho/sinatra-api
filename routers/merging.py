6af12a6088d54b0720683afa2615ce8cb6552adf
Naiim Khaskhoussi <nkhaskho@gmail.com>
2023-08-03 14:47:06 +0100

    Fix merging by adding sep param
    Signed-off-by: Naiim Khaskhoussi <nkhaskho@gmail.com>

--- routers/merging.py
+++ routers/merging.py
@@ -6,13 +6,15 @@ router = APIRouter(prefix="/merging", tags=["merging"])
 
 
 @router.put("/local") # response_model=Dataset, 
-def merge_from_local(dataset1: UploadFile, dataset2: UploadFile, subject_col: str):
+def merge_from_local(dataset1: UploadFile, dataset2: UploadFile, separator: str, subject_col: str):
     """
     TODO: Adding API endpoint documentation
     TODO: Refactor endpoint to merge from remote
     """
-    df1 = pd.read_csv(dataset1.file, sep=";")
-    df2 = pd.read_csv(dataset2.file, sep=";")
+    df1 = pd.read_csv(dataset1.file, sep=separator)
+    df2 = pd.read_csv(dataset2.file, sep=separator)
+    if subject_col=='' or subject_col==None:
+        subject_col = df1.columns[0]
     if subject_col not in df1.columns or subject_col not in df2.columns:
         raise HTTPException(status_code=404, detail="Subject column missing") 
     df_result = pd.merge(df1, df2, how="outer", on=subject_col)
