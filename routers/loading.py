514ad08853ac4dd20ec006bf38604824210628ae
Naiim Khaskhoussi <nkhaskho@gmail.com>
2023-08-03 14:32:10 +0100

    Refactor loading and preprocess
    Signed-off-by: Naiim Khaskhoussi <nkhaskho@gmail.com>

--- routers/loading.py
+++ routers/loading.py
@@ -1,13 +1,24 @@
 import pandas as pd
 from fastapi import APIRouter, UploadFile
 
+ABBREVIATIONS = {
+    "y": 'yes',
+    "n": 'no',
+    "m": 'male',
+    "f": 'femal',
+    "nn": 'naninne',
+    "ch": 'champion',
+}
+
 
 router = APIRouter(prefix="/loading", tags=["loading"])
 
 
 @router.get("/remote") 
-async def preprocess(url: str):
-    df = pd.read_csv(url, sep=";")
+async def preprocess(url: str, separator: str):
+    df = pd.read_csv(url, sep=separator)
+    # Abbreviations replace (in-place)
+    df.replace(ABBREVIATIONS.keys(), ABBREVIATIONS.values(), inplace=True)
     rows = df.to_numpy(na_value='')
     dataset = [list(df.columns.values)]
     dataset.extend([list(row) for row in rows])
@@ -15,10 +26,12 @@ async def preprocess(url: str):
 
 
 @router.post("/local")
-def load_from_upload(file: UploadFile):
+def load_from_upload(file: UploadFile, separator: str):
     if not file.filename.endswith('.csv'):
         return {"error": "invalid file extension"}
-    df = pd.read_csv(file.file, sep=";")
+    df = pd.read_csv(file.file, sep=separator)
+    # Abbreviations replace (in-place)
+    df.replace(ABBREVIATIONS.keys(), ABBREVIATIONS.values(), inplace=True)
     rows = df.to_numpy(na_value='')
     dataset = [list(df.columns.values)]
     dataset.extend([list(row) for row in rows])
--- routers/preprocess.py
+++ routers/preprocess.py
@@ -1,15 +1,6 @@
 import pandas as pd
 from fastapi import APIRouter, UploadFile
 
-ABBREVIATIONS = {
-    "y": 'yes',
-    "n": 'no',
-    "m": 'male',
-    "f": 'femal',
-    "nn": 'naninne',
-    "ch": 'champion',
-}
-
 
 router = APIRouter(prefix="/preprocess", tags=["preprocess"])
 
@@ -17,8 +8,6 @@ router = APIRouter(prefix="/preprocess", tags=["preprocess"])
 @router.get("/remote")
 async def preprocess_from_remote(url: str, separator: str):
     df = pd.read_csv(url, sep=separator)
-    # Abbreviations replace (in-place)
-    df.replace(ABBREVIATIONS.keys(), ABBREVIATIONS.values(), inplace=True)
     rows = df.to_numpy(na_value='')
     dataset = [list(df.columns.values)]
     dataset.extend([list(row) for row in rows])
@@ -30,8 +19,6 @@ def preprocess_from_upload(file: UploadFile, separator: str):
     if not file.filename.endswith('.csv'):
         return {"error": "invalid file extension"}
     df = pd.read_csv(file.file, sep=separator)
-    # Abbreviations replace (in-place)
-    df.replace(ABBREVIATIONS.keys(), ABBREVIATIONS.values(), inplace=True)
     rows = df.to_numpy(na_value='')
     dataset = [list(df.columns.values)]
     dataset.extend([list(row) for row in rows])
