f88c05033bc5e681af96ba6584d547066be67782
Naiim Khaskhoussi <nkhaskho@gmail.com>
2023-09-03 20:36:22 +0100

    Fix change dataset to lowercase
    Signed-off-by: Naiim Khaskhoussi <nkhaskho@gmail.com>

--- routers/loading.py
+++ routers/loading.py
@@ -6,10 +6,23 @@ from settings import ABBREVS
 
 router = APIRouter(prefix="/loading", tags=["loading"])
 
+def lowercase_dataframe(df: pd.DataFrame):
+    cols = []
+    for index, col in enumerate(df.columns):
+        cols.append(col.lower())
+    df.columns = cols
+    for col in df.columns:
+        try:
+            df[col] = df[col].str.lower()
+        except Exception as e:
+            pass
+    return df
+
 
 @router.get("/remote") 
 async def preprocess(url: str, separator: str):
     df = pd.read_csv(url, sep=separator)
+    df = lowercase_dataframe(df)
     # Abbreviations replace (in-place)
     df.replace(ABBREVS.keys(), ABBREVS.values(), inplace=True)
     rows = df.to_numpy(na_value='')
@@ -23,6 +36,7 @@ def load_from_upload(file: UploadFile, separator: str):
     if not file.filename.endswith('.csv'):
         return {"error": "invalid file extension"}
     df = pd.read_csv(file.file, sep=separator)
+    df = lowercase_dataframe(df)
     # Abbreviations replace (in-place)
     df.replace(ABBREVS.keys(), ABBREVS.values(), inplace=True)
     rows = df.to_numpy(na_value='')
