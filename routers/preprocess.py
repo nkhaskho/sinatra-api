22f2ab04c0e5835daf28b0934c00ee77cf1dc05b
Naiim Khaskhoussi <nkhaskho@gmail.com>
2023-09-05 20:07:12 +0100

    Fix preprocess to return FileResponse
    Signed-off-by: Naiim Khaskhoussi <nkhaskho@gmail.com>

--- routers/preprocess.py
+++ routers/preprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from fastapi import APIRouter, UploadFile
+from fastapi.responses import FileResponse
 from settings import ABBREVS, SPEC_CHARS_DICT
 
 
@@ -19,11 +20,14 @@ def lowercase_dataframe(df: pd.DataFrame):
     return df
 
 @router.get("/remote")
-async def preprocess_from_remote(url: str, separator: str):
+async def preprocess_from_remote(url: str, separator: str, fileres: bool=True):
     df = pd.read_csv(url, sep=separator)
     # Abbreviations replace (in-place)
     df = lowercase_dataframe(df)
     df.replace(ABBREVS.keys(), ABBREVS.values(), inplace=True)
+    if fileres == True:
+        df.to_csv('./datasets/dataset.csv')
+        return FileResponse('./datasets/dataset.csv')
     rows = df.to_numpy(na_value='')
     dataset = [list(df.columns.values)]
     dataset.extend([list(row) for row in rows])
@@ -31,7 +35,7 @@ async def preprocess_from_remote(url: str, separator: str):
 
 
 @router.post("/local")
-def preprocess_from_upload(file: UploadFile, separator: str):
+def preprocess_from_upload(file: UploadFile, separator: str, fileres:bool=True):
     if not file.filename.endswith('.csv'):
         return {"error": "invalid file extension"}
     df = pd.read_csv(file.file, sep=separator)
@@ -40,6 +44,9 @@ def preprocess_from_upload(file: UploadFile, separator: str):
     df.replace(ABBREVS.keys(), ABBREVS.values(), inplace=True)
     # add SPEC_CHARS_DICT & fix regex check
     df.replace({r'.*\?.*': ''}, regex=True, inplace=True)
+    if fileres == True:
+        df.to_csv('./datasets/dataset.csv')
+        return FileResponse('./datasets/dataset.csv')
     rows = df.to_numpy(na_value='')
     dataset = [list(df.columns.values)]
     dataset.extend([list(row) for row in rows])
